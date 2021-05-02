a1ae86af239cf7b543e4543868fb6cdb6c9000fe
Meliodas <banktv76@gmail.com>
2021-05-03 06:29:13 +0700

    Dash board

--- dashboard.php
+++ dashboard.php
@@ -2,213 +2,87 @@
     session_start();
     include_once('command/config.php');
 ?>
-
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
-<head>
+  <head>
+    <title>DashBoard</title>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description"
-        content="Responsive sidebar template with sliding effect and dropdown menu based on bootstrap 3">
-    <title>Expense Website</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="custom.css">
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/2b7bd13048.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+      <div class="container-fluid">
+          <div class="row" id="background">
+            <div class="col-2" id="sidebar-menu">
+                <div class="d-flex justify-content-center mt-5" > <h3>Expense manager</h3> </div>
+                <div class="d-flex justify-content-center mt-3"><p>General</p></div>
+                <hr>
+                <div class="d-flex justify-content-start mt-2 d-flex align-items-center" id="sidebar"><i class="fas fa-chart-line mr-4"></i> <a href="dashboard.php" style="color: #181A1B;" >DashBoard</a><i class="fas fa-chevron-right ml-auto"></i></div>
+                <div class="d-flex justify-content-start mt-2 d-flex align-items-center " id="sidebar"><i class="fab fa-bitcoin mr-4"></i><a href="expenses.php"style="color: #181A1B;">Expenses</a><i class="fas fa-chevron-right ml-auto"></i></div>
+                <div class="d-flex justify-content-start mt-2 d-flex align-items-center" id="sidebar"><i class="fas fa-clipboard-list mr-4"></i><a href="expenses_report.php"style="color: #181A1B;">Expenses Report</a><i class="fas fa-chevron-right ml-auto"></i></div>
+                <div class="d-flex justify-content-start mt-2 d-flex align-items-center" id="sidebar"><i class="fas fa-exchange-alt mr-4"></i><a href="transaction.php"style="color: #181A1B;">Transactions</a><i class="fas fa-chevron-right ml-auto"></i></div>
+                <div class="d-flex justify-content-start mt-2 d-flex align-items-center" id="sidebar"><i class="fas fa-clipboard-list mr-4"></i><a href="transaction_report.php"style="color: #181A1B;">Transactions Report</a><i class="fas fa-chevron-right ml-auto"></i></div>
+                <div class="d-flex justify-content-start mt-2 d-flex align-items-center" id="sidebar"><i class="fas fa-cogs mr-4"></i><a href="setting.php"style="color: #181A1B;">Setting</a><i class="fas fa-chevron-right ml-auto"></i></div>
+                <div class="d-flex justify-content-start mt-2 d-flex align-items-center" id="sidebar"><i class="fas fa-sign-out-alt mr-4"></i><a href="Logout.php"style="color: #181A1B;">Logout</a><i class="fas fa-chevron-right ml-auto"></i></div>
+            </div>
+            <div class="col">
 
+            <nav class="navbar navbar-expand-lg navbar-light navback mt-4">
+  <img src="image/user-1.png" alt="profile" width="3%">
 
-</head>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto ">
 
-<body>
-    <div class="container-fluid pl-0">
+      <li class="nav-item ml-3 mt-3">
+        <p class="text-uppercase" style="font-size: 20px "><?php echo $_SESSION['name']?></p>
+      </li>
+
+      <li class="nav-item ml-3 mt-3" >
+        <p class="text-uppercase" style="font-size: 20px"><?php echo $_SESSION['role']?></p>
+      </li>
+
+      <li class="nav-item ml-3 mt-4">
+      <i class="fas fa-cog"  style="font-size: 20px"></i>
+        </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div class="row mt-5 ">
+    <div class="col">
+        <div class="row bg-white ml-1">
+            <div class="col "><p class="text-uppercase bg-primary text-white d-flex justify-content-center">Total Balance</p>
+        </div>
+        </div>
         <div class="row">
-            <div class="col-2">
-                <div class="side-bar">
-                    <p style="font-size: 14.9px;">Expense Website</p>
-                    <img src="image/profile.jpg" alt="" width="50%">
-                    <hr>
-                    <p style="font-size: 14.9px;"><?php echo $_SESSION['name'] ?></p>
-                    <p style="font-size: 14.9px;"><?php echo $_SESSION['role'] ?></p>
-                    <p style="font-size: 12px;"><i class="fas fa-globe-asia" style="color: green;"></i> Online</p>
-                    <hr>
-                    <p style="font-size: 14.9px;">General</p>
-                    <div class="">
-                        <a href="dashboard.php" style="font-size: 14.9px;">DashBoard</a>
-                    </div>
-                    <div class="">
-                        <a href="expenses.php" style="font-size: 14.9px;">Expenses</a>
-                    </div>
-                    <div class="">
-                        <a href="expenses_report.php" style="font-size: 14.9px;">Expenses-Report</a>
-                    </div>
-                    <div class="">
-                        <a href="transaction.php" style="font-size: 14.9px;">Transaction</a>
-                    </div>
-                    <div class="">
-                        <a href="transaction_report.php" style="font-size: 14.9px;">Transaction-Report</a>
-                    </div>
-                    <div class="">
-                        <a href="setting.php" style="font-size: 14.9px;">Setting</a>
-                    </div>
-                    <div class="">
-                        <a href="Logout.php" style="font-size: 14.9px;">Logout</a>
-                    </div>
-
-
-                </div>
-            </div>
-
-
-            <div class="col mt-5 content">
-                <div class="row">
-                    <div class="col">
-                        <div class="total-Balance p-3 mb-2 bg-primary text-white">
-                            <p class="text-center">Total-Balance</p>
-                            <?php 
-                                $sql = "Select u.balance as balance, m.mode_name as mode 
-                                FROM users u CROSS JOIN mode m
-                                 WHERE u.user_id = :user_id AND u.mode = m.mode_id";
-                                $query = $db->prepare($sql);
-                                $query->bindParam(':user_id', $user_id, PDO::PARAM_STR);
-                                $user_id = $_SESSION['user_id'];
-                                $query->execute();
-                                $result = $query->fetchAll(PDO::FETCH_OBJ);
-                                    if($query->rowCount() >0) {
-                                        foreach($result as $res)
-                                        {   
-                            ?>
-                            <p class="text-center"> <?php echo $res->balance ?> ฿</p>
-                            <p class="text-center"> <?php echo $res->mode ?></p>
-
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="total-expense p-3 mb-2 bg-success text-white">
-                            <p class="text-center">Total-Expense</p>
-                            <?php 
-                                $sql = "Select sum(cost) as total_cost FROM expenses WHERE user_id = :user_id";
-                                $query = $db->prepare($sql);
-                                $query->bindParam(':user_id', $user_id, PDO::PARAM_STR);
-                                $user_id = $_SESSION['user_id'];
-                                $query->execute();
-                                $result = $query->fetchAll(PDO::FETCH_OBJ);
-                                    if($query->rowCount() >0) {
-                                        foreach($result as $res1)
-                                        {   
-                            ?>
-                            <p class="text-center"> <?php echo $res1->total_cost ?> ฿</p>
-                        </div>
-                        <?php 
-                            }
-                        }?>
-                    </div>
-                    <div class="col">
-                        <div class="yesterday p-3 mb-2 bg-success text-white">
-                            <p class="text-center">Current Balance</p>
-                            <?php 
-                                $sql = "Select * FROM users WHERE user_id = :user_id";
-                                $query = $db->prepare($sql);
-                                $query->bindParam(':user_id', $user_id, PDO::PARAM_STR);
-                                $user_id = $_SESSION['user_id'];
-                                $query->execute();
-                                $result = $query->fetchAll(PDO::FETCH_OBJ);
-                                    if($query->rowCount() >0) {
-                                        foreach($result as $res2)
-                                        {   
-                            ?>
-                            <p class="text-center"><?php echo $res2->current_balance ?> ฿</p>
-                        </div>
-                        <?php 
-                                        }
-                                    }
-
-                        ?>
-                    </div>
-
-
-                </div>
-
-                <div class="row">
-                    <div class="col">
-                        <div class="yesterday p-3 mb-2 bg-success text-white">
-                            <p class="text-center">Receiving Date</p>
-                            <?php 
-                                $sql = "Select * FROM users WHERE user_id = :user_id";
-                                $query = $db->prepare($sql);
-                                $query->bindParam(':user_id', $user_id, PDO::PARAM_STR);
-                                $user_id = $_SESSION['user_id'];
-                                $query->execute();
-                                $result = $query->fetchAll(PDO::FETCH_OBJ);
-                                    if($query->rowCount() >0) {
-                                        foreach($result as $res2)
-                                        {   
-                            ?>
-                            <p class="text-center"><?php echo $res2->Receiving_date ?> </p>
-                        </div>
-                        <?php 
-                                        }
-                                    }
-
-                        ?>
-                    </div>
-
-                    <div class="col">
-                        <div class="yesterday p-3 mb-2 bg-success text-white">
-                            <p class="text-center">Expiration Date</p>
-                            <?php 
-                                $sql = "Select * FROM users WHERE user_id = :user_id";
-                                $query = $db->prepare($sql);
-                                $query->bindParam(':user_id', $user_id, PDO::PARAM_STR);
-                                $user_id = $_SESSION['user_id'];
-                                $query->execute();
-                                $result = $query->fetchAll(PDO::FETCH_OBJ);
-                                    if($query->rowCount() >0) {
-                                        foreach($result as $res2)
-                                        {   
-                            ?>
-                            <p class="text-center"><?php echo $res2->Expiration_date ?> </p>
-                        </div>
-                        <?php 
-                                        }
-                                    }
-
-                        ?>
-                    </div>
-                </div>
-
-
-                <?php 
-                    }
-                }
-                ?>
-            </div>
+            <div class="col"><p class="bg-white">123</p></div>
         </div>
     </div>
-
-
+    <div class="col">
+        <p class="total text-uppercase d-flex justify-content-center pt-3">Total Expenses</p>
+    </div>
+    <div class="col">
+        <p class="total text-uppercase d-flex justify-content-center pt-3">Current Balance</p>
+    </div>
+</div>
+                </div>
+            </div>
+          </div>
+      </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
-
-    <script>
-
-    </script>
-
-</body>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
